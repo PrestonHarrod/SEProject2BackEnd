c53f3f21cf21bebc7de1550751ba81d1121fa2de
PrestonHarrod <prestonharrod@gmail.com>
2021-09-03 10:11:27 -0500

    add rest requests

--- routes/courses.js
+++ routes/courses.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   if (req.query.per_page == null) limit = 20;
   else limit = parseInt(req.query.per_page);
   res.locals.connection.query(
-    "SELECT * FROM courses LIMIT ? OFFSET ?",
+    "SELECT * FROM courses LIMIT 5000 OFFSET ?",
     [limit, offset],
     function(error, results, fields) {
       if (error) {
@@ -25,6 +25,103 @@ router.get('/', function(req, res, next) {
       res.locals.connection.end();
     }
   );
+});
+router.get("/:id", function(req, res, next) {
+  var id = req.params.id;
+  res.locals.connection.query("SELECT * FROM courses WHERE id=?", id, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.status(500);
+      res.send(JSON.stringify({ status: 500, error: error, response: null }));
+      //If there is error, we send the error in the error section with 500 status
+    } else {
+      res.status(200);
+      res.send(JSON.stringify(results));
+      //If there is no error, all is good and response is 200OK.
+    }
+    res.locals.connection.end();
+  });
+});
+router.put("/:id", function(req, res, next) {
+  var id = req.params.id;
+  var student = req.body;
+  let errorMessage = validate(student);
+  if (errorMessage.length > 2) {
+    res.status(406);
+    res.send(errorMessage);
+  } else {
+    res.locals.connection.query(
+      "UPDATE courses SET ? WHERE id=?",
+      [req.body, id],
+      function(error, results) {
+        if (error) {
+          res.status(500);
+          res.send(
+            JSON.stringify({ status: 500, error: error, response: null })
+          );
+          //If there is error, we send the error in the error section with 500 status
+        } else {
+          res.status(200);
+          res.send(
+            JSON.stringify({ status: 200, error: null, response: results })
+          );
+          //If there is no error, all is good and response is 200OK.
+        }
+        res.locals.connection.end();
+      }
+    );
+  }
+});
+router.post("/", function(req, res, next) {
+  var student = req.body;
+  let errorMessage = validate(student);
+  if (errorMessage.length > 2) {
+    res.status(406);
+    res.send(errorMessage);
+  } else {
+    res.locals.connection.query(
+      "INSERT INTO courses SET ? ",
+      req.body,
+      function(error, results) {
+        if (error) {
+          res.status(500);
+          res.send(
+            JSON.stringify({ status: 500, error: error, response: null })
+          );
+          //If there is error, we send the error in the error section with 500 status
+        } else {
+          res.status(200);
+          res.send(
+            JSON.stringify({ status: 200, error: null, response: results })
+          );
+          //If there is no error, all is good and response is 200OK.
+        }
+        res.locals.connection.end();
+      }
+    );
+  }
+});
+
+router.delete("/:id", function(req, res, next) {
+  var id = req.params.id;
+  res.locals.connection.query("DELETE FROM courses WHERE id=?", id, function(
+    error,
+    results
+  ) {
+    if (error) {
+      res.status = 500;
+      res.send(JSON.stringify({ status: 500, error: error, response: null }));
+      //If there is error, we send the error in the error section with 500 status
+    } else {
+      res.status = 200;
+      res.send(JSON.stringify({ status: 200, error: null, response: results }));
+      //If there is no error, all is good and response is 200OK.
+    }
+    res.locals.connection.end();
+  });
 });
 
 module.exports = router;
