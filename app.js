785ea7562bdb72788989f3d30c1eb6df30399239
PrestonHarrod <prestonharrod@gmail.com>
2021-09-15 20:45:52 -0500

    works with local build and maybe production

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/courses');
 var app = express();
 
 var corsOptions = {
-  origin: 'http://localhost:8081',
+  origin: '*',
 }
 
 
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 app.options('*', cors());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Access-Control-Allow-Origin');
   res.setHeader('Access-Control-Allow-Credentials', true);
